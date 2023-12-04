461ed1a2c7733a95ccfe702c6067bc72a97f33f3
Shekhar67907 <124515969+Shekhar67907@users.noreply.github.com>
2023-12-04 21:54:46 +0530

    Update medic_cia.py

--- medic_cia.py
+++ medic_cia.py
@@ -1,153 +1,78 @@
 import streamlit as st
-from streamlit_chat import message as st_message
-from audiorecorder import audiorecorder
+from audiorecorder import audiorecorder  # Assuming you have a library for audio recording
+
 import time
 import json
 import requests
 
 
-token_hugging_face = "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"
+token_hugging_face = "Your token access"
 
 headers = {"Authorization": f"Bearer {token_hugging_face}"} #TOKEN HUGGING FACE
 API_URL_RECOGNITION = "https://api-inference.huggingface.co/models/openai/whisper-tiny.en"
 API_URL_DIAGNOSTIC = "https://api-inference.huggingface.co/models/abhirajeshbhai/symptom-2-disease-net"
 
 
+
 def recognize_speech(audio_file):
-
     with open(audio_file, "rb") as f:
-
         data = f.read()
 
-    time.sleep(1)
-
-    while True:
-            
+    retries = 3
+    for _ in range(retries):
         try:
-
-            response = requests.request("POST", API_URL_RECOGNITION, headers=headers, data=data)
-
+            response = requests.request("POST", API_URL_RECOGNITION, headers=headers, data=data, timeout=10)
+            response.raise_for_status()
             output = json.loads(response.content.decode("utf-8"))
-
             final_output = output['text']
+            return final_output
+        except (KeyError, requests.RequestException) as e:
+            print(f"Error in recognize_speech: {e}")
+            time.sleep(1)
 
-            break
+    return "Failed to recognize speech"
 
-        except KeyError:
-
-            continue
-
-    return final_output
-
-
+# Disease prediction model
 def diagnostic_medic(voice_text):
-
     synthomps = {"inputs": voice_text}
-
     data = json.dumps(synthomps)
 
-      
-
-    time.sleep(1)
-
-    while True:
-
+    retries = 3
+    for _ in range(retries):
         try:
-
-            response = requests.request("POST", API_URL_DIAGNOSTIC, headers=headers, data=data)  
-
+            response = requests.request("POST", API_URL_DIAGNOSTIC, headers=headers, data=data, timeout=10)
+            response.raise_for_status()
             output = json.loads(response.content.decode("utf-8"))
-
             final_output = output[0][0]['label']
+            return final_output
+        except (KeyError, requests.RequestException) as e:
+            print(f"Error in diagnostic_medic: {e}")
+            time.sleep(1)
 
-            break
+    return "Failed to diagnose"
 
-        except KeyError:
+# Paste the recognize_speech and diagnostic_medic functions here
 
-            continue
-
-    return final_output
-
-
-
-def generate_answer(audio):
-
-
-
-    with st.spinner("Consultation in progress..."):
-
-        # To save audio to a file:
-        wav_file = open("audio.wav", "wb")
-
-        wav_file.write(audio.tobytes())
-                
-        # Voice recognition model
-        
-        text = recognize_speech("./audio.wav")
-
-
-        #Disease Prediction Model
-
-        diagnostic = diagnostic_medic(text)
- 
-
-        #Save conversation
-        st.session_state.history.append({"message": text, "is_user": True})
-        st.session_state.history.append({"message": f" Your disease would be {diagnostic}", "is_user": False})
-
-
-        st.success("Medical consultation done") 
-
-           
-
-
-
-if __name__ == "__main__":
-    
-
-    # remove the hamburger in the upper right hand corner and the Made with Streamlit footer
-    hide_menu_style = """
-        <style>
-        #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-        </style>
-        """
-    st.markdown(hide_menu_style, unsafe_allow_html=True)
-
-        
-    col1, col2, col3 = st.columns(3)
-
-    with col1:
-        st.write(' ')
-
-        
-    with col2:
-        st.image("./logo_.png", width = 200)
-
-        
-    with col3:
-        st.write(' ')
-
-    
-    if "history" not in st.session_state:
-
-        st.session_state.history = []
-
+# Streamlit app
+def main():
     st.title("Medical Diagnostic Assistant")
 
-    
-    #Show Input
+    # User interface for recording audio
     audio = audiorecorder("Start recording", "Recording in progress...")
 
     if len(audio) > 0:
+        st.success("Recording complete!")
 
-        generate_answer(audio)
+        # Voice recognition and disease prediction
+        st.subheader("Analyzing... Please wait.")
+        with st.spinner("Analyzing audio..."):
+            text_result = recognize_speech(audio)
+            disease_result = diagnostic_medic(text_result)
 
-        for i, chat in enumerate(st.session_state.history): #Show historical consultation
+        # Display results
+        st.subheader("Results:")
+        st.write(f"Recognized Symptomps: {text_result}")
+        st.write(f"Predicted Disease: {disease_result}")
 
-            st_message(**chat, key =str(i))
-
-
-
-
-       
+if __name__ == "__main__":
+    main()
