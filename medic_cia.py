6e7a7bcca565095ced51b896b38f124e3f40850d
Shekhar67907 <124515969+Shekhar67907@users.noreply.github.com>
2023-12-21 02:15:26 +0530

    Update medic_cia.py

--- medic_cia.py
+++ medic_cia.py
@@ -1,121 +1,128 @@
 import requests
 from pydub import AudioSegment
 import streamlit as st
+from streamlit_chat import message as st_message
 from audiorecorder import audiorecorder
+import json
 import time
 
-class APIConfig:
-    # API details for Speech Recognition
-    RECOGNITION_URL = "https://api-inference.huggingface.co/models/openai/whisper-large-v2"
-    RECOGNITION_HEADERS = {"Authorization": "Bearer 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"}
+# Updated API details for Speech Recognition
+API_URL_RECOGNITION = "https://api-inference.huggingface.co/models/openai/whisper-large-v2"
+headers = {"Authorization": "Bearer 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"}
 
-    # API details for Disease Prediction Models
-    DIAGNOSTIC_URL_1 = "https://api-inference.huggingface.co/models/abhirajeshbhai/symptom-2-disease-net"
-    DIAGNOSTIC_URL_2 = "https://api-inference.huggingface.co/models/DinaSalama/symptom_to_disease_distb"
-    DIAGNOSTIC_HEADERS = {"Authorization": "Bearer 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"}
-
-def handle_api_response(response, retry_function, *args):
-    if response.status_code == 503:  # HTTP 503 Service Unavailable
-        estimated_time = response.json().get('estimated_time', 20.0)
-        st.warning(
-            f"Model is currently loading. Please wait for approximately {estimated_time:.2f} seconds and try again.")
-        time.sleep(estimated_time)
-        return retry_function(*args)  # Retry after waiting
-
-    return response.json()
+# Updated API details for Disease Prediction
+API_URL_DIAGNOSTIC = "https://api-inference.huggingface.co/models/abhirajeshbhai/symptom-2-disease-net"
+headers_diagnostic = {"Authorization": "Bearer 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"}
 
 def recognize_speech(audio_file):
     with open(audio_file, "rb") as f:
         data = f.read()
 
-    response = requests.post(APIConfig.RECOGNITION_URL, headers=APIConfig.RECOGNITION_HEADERS, data=data)
+    response = requests.post(API_URL_RECOGNITION, headers=headers, data=data)
+
+    if response.status_code == 503:  # HTTP 503 Service Unavailable
+        estimated_time = response.json().get('estimated_time', 50.0)
+        st.warning(
+            f"Model is currently loading. Please wait for approximately {estimated_time:.2f} seconds and try again.")
+        time.sleep(estimated_time)
+        return recognize_speech(audio_file)  # Retry after waiting
 
     if response.status_code != 200:
         st.error(f"Speech recognition API error: {response.content}")
-        return {"text": "Speech recognition failed"}
+        return "Speech recognition failed"
 
-    return handle_api_response(response, recognize_speech, audio_file)
-
-def query_diagnostic_with_retry(payload, api_url):
-    response = requests.post(api_url, headers=APIConfig.DIAGNOSTIC_HEADERS, json=payload)
-    return handle_api_response(response, query_diagnostic_with_retry, payload, api_url)
-
-def format_diagnostic_results(results):
-    if results and isinstance(results, list):
-        sorted_results = sorted(results, key=lambda x: x.get('score', 0) if isinstance(x, dict) else 0, reverse=True)
-        formatted_results = [result.get('label', 'Unknown Disease') if isinstance(result, dict) and 'label' in result else 'Unknown Disease' for result in sorted_results[:2]]
-        return f'Top Diseases:\n{", ".join(formatted_results)}'
-
-    return 'No diagnostic information available'
+    output = response.json()
+    final_output = output.get('text', 'Speech recognition failed')
+    return final_output
 
 def diagnostic_medic(voice_text):
     payload = {"inputs": voice_text}
+    response = query_diagnostic(payload)  # Using the new API for disease prediction
 
-    # Query the first diagnostic model
-    response_1 = query_diagnostic_with_retry(payload, APIConfig.DIAGNOSTIC_URL_1)
-    confidence_1 = response_1[0].get('score', 0.0) if isinstance(response_1, list) and response_1 else 0.0
+    try:
+        # Extract top diseases based on the model's output
+        top_results = response.get('predictions', [])
+        final_output = format_diagnostic_results(top_results)
+    except (KeyError, IndexError):
+        final_output = 'Diagnostic information not available'
 
-    # Query the second diagnostic model
-    response_2 = query_diagnostic_with_retry(payload, APIConfig.DIAGNOSTIC_URL_2)
-    confidence_2 = response_2[0].get('score', 0.0) if isinstance(response_2, list) and response_2 else 0.0
+    return final_output
 
-    # Compare confidence scores and determine the final diagnostic result
-    if confidence_1 > confidence_2:
-        final_results = response_1
-    else:
-        final_results = response_2
+def format_diagnostic_results(results):
+    # Sort the results based on the score in descending order
+    sorted_results = sorted(results, key=lambda x: x['score'], reverse=True)
 
-    # Extract the symptoms with their confidence levels
-    if isinstance(final_results, list) and final_results:
-        symptoms_with_confidence = [(result.get('label', 'Unknown Symptom'), result.get('score', 0.0)) for result in final_results]
-    else:
-        symptoms_with_confidence = []
+    # Extract the names of the top 2 diseases
+    top_results = sorted_results[:2]
+    formatted_results = [result['label'] for result in top_results]
 
-    # Sort symptoms by confidence level in descending order
-    sorted_symptoms = sorted(symptoms_with_confidence, key=lambda x: x[1], reverse=True)
+    if not formatted_results:
+        return 'No diagnostic information available'
 
-    # Extract the symptom with the highest confidence
-    top_symptom, top_confidence = sorted_symptoms[0] if sorted_symptoms else ('Unknown Symptom', 0.0)
+    return f'Top Diseases:\n{", ".join(formatted_results)}'
 
-    return f'Highest Confidence Symptom: {top_symptom} (Confidence: {top_confidence:.2f})'
-
-# Rest of the code remains unchanged
-
-
+def query_diagnostic(payload):
+    response = requests.post(API_URL_DIAGNOSTIC, headers=headers_diagnostic, json=payload)
+    return response.json()
 
 def generate_answer(audio_recording):
     st.spinner("Consultation in progress...")
 
-    audio_recording.export("audio.flac", format="flac")
+    # To save audio to a file:
+    audio_recording.export("audio.flac", format="flac")  # Save as FLAC for Whisper ASR
 
+    # Voice recognition model
     st.write("Audio file saved. Starting speech recognition...")
-    recognition_result = recognize_speech("audio.flac")
+    text = recognize_speech("audio.flac")  # Use Whisper ASR for speech recognition
 
-    # Check if speech recognition was successful
-    if "recognition failed" in recognition_result.get('text', '').lower():
+    if "recognition failed" in text.lower():
         st.error("Voice recognition failed. Please try again.")
         return
 
-    # Extract the transcribed text from the recognition result
-    text = recognition_result.get('text', 'Unknown Text')
-
     st.write(f"Speech recognition result: {text}")
 
-    st.write("Calling diagnostic models...")
+    # Disease Prediction Model
+    st.write("Calling diagnostic model...")
     diagnostic = diagnostic_medic(text)
     st.write(f"Diagnostic result:\n{diagnostic}")
 
-    if "history" not in st.session_state:
-        st.session_state.history = []
-
-    st.session_state.history.extend([
-        {"message": text, "is_user": True},
-        {"message": diagnostic, "is_user": False}
-    ])
+    # Save conversation
+    st.session_state.history.append({"message": text, "is_user": True})
+    st.session_state.history.append({"message": diagnostic, "is_user": False})
 
     st.success("Medical consultation done")
 
 if __name__ == "__main__":
+    # Remove the hamburger in the upper right-hand corner and the Made with Streamlit footer
+    hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        </style>
+    """
+    st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.write(' ')
+
+    with col2:
+        st.image("./logo_.png", width=200)
+
+    with col3:
+        st.write(' ')
+
+    if "history" not in st.session_state:
+        st.session_state.history = []
+
     st.title("Medical Diagnostic Assistant")
+
+    # Show Input
     audio = audiorecorder("Start recording", "Recording in progress...")
-    generate_answer(audio)
+
+    if audio:
+        generate_answer(audio)
+
+        for i, chat in enumerate(st.session_state.history):  # Show historical consultation
+            st_message(**chat, key=str(i))
