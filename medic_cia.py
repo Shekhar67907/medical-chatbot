e9fd1ba3ed11ff92f150b6665c26bf5085a0119b
Shekhar67907 <124515969+Shekhar67907@users.noreply.github.com>
2023-12-21 02:03:11 +0530

    Update medic_cia.py

--- medic_cia.py
+++ medic_cia.py
@@ -1,7 +1,6 @@
 import requests
 from pydub import AudioSegment
 import streamlit as st
-from streamlit_chat import message as st_message
 from audiorecorder import audiorecorder
 import time
 
@@ -33,7 +32,7 @@ def recognize_speech(audio_file):
 
     if response.status_code != 200:
         st.error(f"Speech recognition API error: {response.content}")
-        return "Speech recognition failed"
+        return {"text": "Speech recognition failed"}
 
     return handle_api_response(response, recognize_speech, audio_file)
 
@@ -52,14 +51,30 @@ def format_diagnostic_results(results):
 def diagnostic_medic(voice_text):
     payload = {"inputs": voice_text}
 
+    # Query the first diagnostic model
     response_1 = query_diagnostic_with_retry(payload, APIConfig.DIAGNOSTIC_URL_1)
     confidence_1 = response_1[0].get('score', 0.0) if response_1 else 0.0
 
+    # Query the second diagnostic model
     response_2 = query_diagnostic_with_retry(payload, APIConfig.DIAGNOSTIC_URL_2)
     confidence_2 = response_2[0].get('score', 0.0) if response_2 else 0.0
 
-    final_results = response_1 if confidence_1 > confidence_2 else response_2
-    return format_diagnostic_results(final_results)
+    # Compare confidence scores and determine the final diagnostic result
+    if confidence_1 > confidence_2:
+        final_results = response_1
+    else:
+        final_results = response_2
+
+    # Extract the symptoms with their confidence levels
+    symptoms_with_confidence = [(result.get('label', 'Unknown Symptom'), result.get('score', 0.0)) for result in final_results] if final_results else []
+
+    # Sort symptoms by confidence level in descending order
+    sorted_symptoms = sorted(symptoms_with_confidence, key=lambda x: x[1], reverse=True)
+
+    # Extract the symptom with the highest confidence
+    top_symptom, top_confidence = sorted_symptoms[0] if sorted_symptoms else ('Unknown Symptom', 0.0)
+
+    return f'Highest Confidence Symptom: {top_symptom} (Confidence: {top_confidence:.2f})'
 
 def generate_answer(audio_recording):
     st.spinner("Consultation in progress...")
@@ -67,11 +82,15 @@ def generate_answer(audio_recording):
     audio_recording.export("audio.flac", format="flac")
 
     st.write("Audio file saved. Starting speech recognition...")
-    text = recognize_speech("audio.flac")
+    recognition_result = recognize_speech("audio.flac")
 
-    if "recognition failed" in text.lower():
+    # Check if speech recognition was successful
+    if "recognition failed" in recognition_result.get('text', '').lower():
         st.error("Voice recognition failed. Please try again.")
         return
+
+    # Extract the transcribed text from the recognition result
+    text = recognition_result.get('text', 'Unknown Text')
 
     st.write(f"Speech recognition result: {text}")
 
@@ -90,24 +109,6 @@ def generate_answer(audio_recording):
     st.success("Medical consultation done")
 
 if __name__ == "__main__":
-    hide_menu_style = """
-        <style>
-        #MainMenu {visibility: hidden;}
-        footer {visibility: hidden;}
-        </style>
-    """
-    st.markdown(hide_menu_style, unsafe_allow_html=True)
-
-    col1, col2, col3 = st.columns(3)
-
-    with col1:
-        st.write(' ')
-
-    with col2:
-        st.image("./logo_.png", width=200)
-
-    with col3:
-        st.write(' ')
-
+    st.title("Medical Diagnostic Assistant")
     audio = audiorecorder("Start recording", "Recording in progress...")
     generate_answer(audio)
