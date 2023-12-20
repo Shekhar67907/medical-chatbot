75ffc2c2458062810ede7867d371778a519aa21f
Shekhar67907 <124515969+Shekhar67907@users.noreply.github.com>
2023-12-20 23:51:24 +0530

    Update medic_cia.py

--- medic_cia.py
+++ medic_cia.py
@@ -10,8 +10,9 @@ import time
 API_URL_RECOGNITION = "https://api-inference.huggingface.co/models/openai/whisper-large-v2"
 headers = {"Authorization": "Bearer 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"}
 
-# Updated API details for Disease Prediction
-API_URL_DIAGNOSTIC = "https://api-inference.huggingface.co/models/abhirajeshbhai/symptom-2-disease-net"
+# API details for Disease Prediction Models
+API_URL_DIAGNOSTIC_1 = "https://api-inference.huggingface.co/models/abhirajeshbhai/symptom-2-disease-net"
+API_URL_DIAGNOSTIC_2 = "https://api-inference.huggingface.co/models/DinaSalama/symptom_to_disease_distb"
 headers_diagnostic = {"Authorization": "Bearer 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"}
 
 def recognize_speech(audio_file):
@@ -35,18 +36,9 @@ def recognize_speech(audio_file):
     final_output = output.get('text', 'Speech recognition failed')
     return final_output
 
-def diagnostic_medic(voice_text):
-    payload = {"inputs": voice_text}
-    response = query_diagnostic(payload)  # Using the new API for disease prediction
-
-    try:
-        # Extract top diseases based on the model's output
-        top_results = response.get('predictions', [])
-        final_output = format_diagnostic_results(top_results)
-    except (KeyError, IndexError):
-        final_output = 'Diagnostic information not available'
-
-    return final_output
+def query_diagnostic(payload, api_url):
+    response = requests.post(api_url, headers=headers_diagnostic, json=payload)
+    return response.json()
 
 def format_diagnostic_results(results):
     # Sort the results based on the score in descending order
@@ -61,9 +53,32 @@ def format_diagnostic_results(results):
 
     return f'Top Diseases:\n{", ".join(formatted_results)}'
 
-def query_diagnostic(payload):
-    response = requests.post(API_URL_DIAGNOSTIC, headers=headers_diagnostic, json=payload)
-    return response.json()
+def diagnostic_medic(voice_text):
+    payload = {"inputs": voice_text}
+
+    # Query the first diagnostic model
+    response_1 = query_diagnostic(payload, API_URL_DIAGNOSTIC_1)
+    try:
+        top_results_1 = response_1.get('predictions', [])
+        confidence_1 = top_results_1[0]['score']
+    except (KeyError, IndexError):
+        confidence_1 = 0.0
+
+    # Query the second diagnostic model
+    response_2 = query_diagnostic(payload, API_URL_DIAGNOSTIC_2)
+    try:
+        top_results_2 = response_2.get('predictions', [])
+        confidence_2 = top_results_2[0]['score']
+    except (KeyError, IndexError):
+        confidence_2 = 0.0
+
+    # Compare confidence scores and determine the final diagnostic result
+    if confidence_1 > confidence_2:
+        final_results = top_results_1
+    else:
+        final_results = top_results_2
+
+    return format_diagnostic_results(final_results)
 
 def generate_answer(audio_recording):
     st.spinner("Consultation in progress...")
@@ -82,7 +97,7 @@ def generate_answer(audio_recording):
     st.write(f"Speech recognition result: {text}")
 
     # Disease Prediction Model
-    st.write("Calling diagnostic model...")
+    st.write("Calling diagnostic models...")
     diagnostic = diagnostic_medic(text)
     st.write(f"Diagnostic result:\n{diagnostic}")
 
