097462a67afa5897c36ec3f763df1cbfd4810402
Shekhar67907 <124515969+Shekhar67907@users.noreply.github.com>
2023-12-23 15:31:06 +0530

    Update medic_cia.py

--- medic_cia.py
+++ medic_cia.py
@@ -27,7 +27,7 @@ def recognize_speech(audio_file):
         estimated_time = response.json().get('estimated_time', 50.0)
         st.warning(
             f"Model is currently loading. Please wait for approximately {estimated_time:.2f} seconds and try again.")
-        time.sleep(estimated_time)
+        time.sleep(10)
         return recognize_speech(audio_file)  # Retry after waiting
 
     if response.status_code != 200:
