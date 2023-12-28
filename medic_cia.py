f13ce03ab3d63605a1818a6a6cba8df6f6b1d637
Shekhar67907 <124515969+Shekhar67907@users.noreply.github.com>
2023-12-28 22:57:38 +0530

    Update medic_cia.py

--- medic_cia.py
+++ medic_cia.py
@@ -69,10 +69,42 @@ def recognize_speech(audio):
     return final_output
 
 def diagnostic_medic(voice_text):
-    # ... (unchanged)
+    model_results = []
+
+    for model_info in DIAGNOSTIC_MODELS:
+        payload = {"inputs": [voice_text]}
+        response = requests.post(model_info["api_url"], headers=headers, json=payload)
+
+        try:
+            results = response.json()[0][:5]
+            model_results.append({"name": model_info["name"], "results": results})
+        except (KeyError, IndexError):
+            st.warning(f'Diagnostic information not available for {model_info["name"]}')
+
+    if not model_results:
+        return 'No diagnostic information available'
+
+    # Compare results based on confidentiality score and choose the model with the highest score
+    best_model_result = max(model_results, key=lambda x: max([result['score'] for result in x['results']], default=0.0))
+    
+    return format_diagnostic_results(best_model_result["results"], best_model_result["name"])
+
 
 def format_diagnostic_results(results, model_name):
-    # ... (unchanged)
+    # Sort the results based on the score in descending order
+    sorted_results = sorted(results, key=lambda x: x['score'], reverse=True)
+
+    # Extract the names and scores of the top results
+    top_results = sorted_results[:2]
+    formatted_results = [(result['label'], result['score']) for result in top_results]
+
+    if not formatted_results:
+        return 'No diagnostic information available'
+
+    # Create a string with disease names and confidence scores
+    formatted_results_str = ', '.join([f'{label} ({score:.2%})' for label, score in formatted_results])
+
+    return f'Top Diseases or Symptoms from {model_name}:\n{formatted_results_str}'
 
 def generate_answer(audio_recording):
     st.spinner("Consultation in progress...")
