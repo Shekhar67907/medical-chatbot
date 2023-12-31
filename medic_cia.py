5412d8fcda05246095c3314eabae26316f0b12ea
Shekhar67907 <124515969+Shekhar67907@users.noreply.github.com>
2023-12-31 15:53:51 +0530

    Update medic_cia.py

--- medic_cia.py
+++ medic_cia.py
@@ -1,14 +1,22 @@
 import requests
+from pydub import AudioSegment
 import streamlit as st
-from audiorecorder import audiorecorder
 from streamlit_chat import message as st_message
+from audiorecorder import audiorecorder
+import json
 import time
 
-
-
+# Updated API details
 API_URL_RECOGNITION = "https://api-inference.huggingface.co/models/jonatasgrosman/wav2vec2-large-xlsr-53-english"
-API_URL_DIAGNOSTIC = "https://api-inference.huggingface.co/models/shanover/medbot_godel_v3"
+
+# List of diagnostic models with their respective API URLs
+DIAGNOSTIC_MODELS = [
+    {"name": "Model 1", "api_url": "https://api-inference.huggingface.co/models/abhirajeshbhai/symptom-2-disease-net"},
+    {"name": "Model 2", "api_url": "https://api-inference.huggingface.co/models/DinaSalama/symptom_to_disease_distb"},
+]
+
 headers = {"Authorization": "Bearer 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"}
+
 
 def recognize_speech(audio_file):
     with open(audio_file, "rb") as f:
@@ -31,9 +39,45 @@ def recognize_speech(audio_file):
     final_output = output.get('text', 'Speech recognition failed')
     return final_output
 
-def query_diagnostic(payload):
-    response = requests.post(API_URL_DIAGNOSTIC, headers=headers, json=payload)
-    return response.json()
+
+def diagnostic_medic(voice_text):
+    model_results = []
+
+    for model_info in DIAGNOSTIC_MODELS:
+        payload = {"inputs": [voice_text]}
+        response = requests.post(model_info["api_url"], headers=headers, json=payload)
+
+        try:
+            results = response.json()[0][:5]
+            model_results.append({"name": model_info["name"], "results": results})
+        except (KeyError, IndexError):
+            st.warning(f'Diagnostic information not available for {model_info["name"]}')
+
+    if not model_results:
+        return 'No diagnostic information available'
+
+    # Compare results based on confidentiality score and choose the model with the highest score
+    best_model_result = max(model_results, key=lambda x: max([result['score'] for result in x['results']], default=0.0))
+
+    return format_diagnostic_results(best_model_result["results"], best_model_result["name"])
+
+
+def format_diagnostic_results(results, model_name):
+    # Sort the results based on the score in descending order
+    sorted_results = sorted(results, key=lambda x: x['score'], reverse=True)
+
+    # Extract the names and scores of the top results
+    top_results = sorted_results[:2]
+    formatted_results = [(result['label'], result['score']) for result in top_results]
+
+    if not formatted_results:
+        return 'No diagnostic information available'
+
+    # Create a string with disease names and confidence scores
+    formatted_results_str = ', '.join([f'{label} ({score:.2%})' for label, score in formatted_results])
+
+    return f'Top Diseases or Symptoms from {model_name}:\n{formatted_results_str}'
+
 
 def generate_answer(audio_recording):
     st.spinner("Consultation in progress...")
@@ -51,15 +95,20 @@ def generate_answer(audio_recording):
 
     st.write(f"Speech recognition result: {text}")
 
-    # Query the diagnostic model
-    st.write("Calling diagnostic model...")
-    diagnostic = query_diagnostic({"inputs": text})
+    # Disease Prediction Model
+    st.write("Calling diagnostic models...")
+    diagnostic = diagnostic_medic(text)
     st.write(f"Diagnostic result:\n{diagnostic}")
 
     # Add the statement for more detailed symptoms
     st.write("Please provide more detailed symptoms for precise recognition.")
 
+    # Save conversation
+    st.session_state.history.append({"message": text, "is_user": True})
+    st.session_state.history.append({"message": diagnostic, "is_user": False})
+
     st.success("Medical consultation done")
+
 
 if __name__ == "__main__":
     # Remove the hamburger in the upper right-hand corner and the Made with Streamlit footer
