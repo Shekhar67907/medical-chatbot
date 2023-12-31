d08c40a79b913d4b392d28e55afe6a656c57e130
Shekhar67907 <124515969+Shekhar67907@users.noreply.github.com>
2023-12-31 14:39:35 +0530

    Update medic_cia.py

--- medic_cia.py
+++ medic_cia.py
@@ -6,10 +6,9 @@ from audiorecorder import audiorecorder
 import json
 import time
 
-# Updated API details
 API_URL_RECOGNITION = "https://api-inference.huggingface.co/models/jonatasgrosman/wav2vec2-large-xlsr-53-english"
+API_URL_DIAGNOSTIC = "https://api-inference.huggingface.co/models/shanover/medbot_godel_v3"
 
-# List of diagnostic models with their respective API URLs
 DIAGNOSTIC_MODELS = [
     {"name": "Model 1", "api_url": "https://api-inference.huggingface.co/models/abhirajeshbhai/symptom-2-disease-net"},
     {"name": "Model 2", "api_url": "https://api-inference.huggingface.co/models/DinaSalama/symptom_to_disease_distb"},
@@ -17,6 +16,9 @@ DIAGNOSTIC_MODELS = [
 
 headers = {"Authorization": "Bearer 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"}
 
+def query_diagnostic(payload):
+    response = requests.post(API_URL_DIAGNOSTIC, headers=headers, json=payload)
+    return response.json()
 
 def recognize_speech(audio_file):
     with open(audio_file, "rb") as f:
@@ -39,6 +41,20 @@ def recognize_speech(audio_file):
     final_output = output.get('text', 'Speech recognition failed')
     return final_output
 
+def get_precautions(disease):
+    # Define precautions for each disease
+    precautions_dict = {
+        "peptic_ulcer": ["Drink plenty of water.", "Avoid spicy and acidic foods.", "Take prescribed medications."],
+        # Add more diseases and their precautions as needed
+    }
+
+    # Check if precautions are available for the predicted disease
+    precautions = precautions_dict.get(disease.lower())
+
+    if precautions:
+        return precautions
+    else:
+        return ["Precautions not available for this disease."]
 
 def diagnostic_medic(voice_text):
     model_results = []
@@ -61,7 +77,6 @@ def diagnostic_medic(voice_text):
 
     return format_diagnostic_results(best_model_result["results"], best_model_result["name"])
 
-
 def format_diagnostic_results(results, model_name):
     # Sort the results based on the score in descending order
     sorted_results = sorted(results, key=lambda x: x['score'], reverse=True)
@@ -77,7 +92,6 @@ def format_diagnostic_results(results, model_name):
     formatted_results_str = ', '.join([f'{label} ({score:.2%})' for label, score in formatted_results])
 
     return f'Top Diseases or Symptoms from {model_name}:\n{formatted_results_str}'
-
 
 def generate_answer(audio_recording):
     st.spinner("Consultation in progress...")
@@ -96,22 +110,35 @@ def generate_answer(audio_recording):
     st.write(f"Speech recognition result: {text}")
 
     # Disease Prediction Model
-    st.write("Calling diagnostic models...")
-    diagnostic = diagnostic_medic(text)
-    st.write(f"Diagnostic result:\n{diagnostic}")
+    st.write("Calling diagnostic model...")
+    diagnostic_result = query_diagnostic({"inputs": text})
+
+    # Extract the predicted disease
+    predicted_disease = diagnostic_result.get('output', 'Unknown').lower()
+
+    st.write(f"Predicted Disease: {predicted_disease}")
+
+    # Get precautions for the predicted disease
+    precautions = get_precautions(predicted_disease)
+
+    # Display precautions
+    st.write("Precautions:")
+    for precaution in precautions:
+        st.write(f"- {precaution}")
 
     # Add the statement for more detailed symptoms
     st.write("Please provide more detailed symptoms for precise recognition.")
 
     # Save conversation
     st.session_state.history.append({"message": text, "is_user": True})
-    st.session_state.history.append({"message": diagnostic, "is_user": False})
+    st.session_state.history.append({"message": f"Predicted Disease: {predicted_disease}", "is_user": False})
+    st.session_state.history.append({"message": "Precautions:", "is_user": False})
+    for precaution in precautions:
+        st.session_state.history.append({"message": f"- {precaution}", "is_user": False})
 
     st.success("Medical consultation done")
 
-
 if __name__ == "__main__":
-    # Remove the hamburger in the upper right-hand corner and the Made with Streamlit footer
     hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
